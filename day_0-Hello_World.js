fa33665572e8196dac2e08cfd476902caed5cd33
Eduardo Rosillo <31942279+edrosillo@users.noreply.github.com>
2023-02-06 08:51:58 -0500

    Add files via upload

---
+++ day_0-Hello_World.js
@@ -0,0 +1,21 @@
+{\rtf1\ansi\ansicpg1252\cocoartf2708
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fnil\fcharset0 Menlo-Regular;}
+{\colortbl;\red255\green255\blue255;\red0\green0\blue255;\red255\green255\blue254;\red0\green0\blue0;
+\red22\green107\blue41;\red144\green1\blue18;}
+{\*\expandedcolortbl;;\cssrgb\c0\c0\c100000;\cssrgb\c100000\c100000\c99608;\cssrgb\c0\c0\c0;
+\cssrgb\c7451\c48627\c21176;\cssrgb\c63922\c8235\c8235;}
+\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+\deftab720
+\pard\pardeftab720\partightenfactor0
+
+\f0\fs30 \cf2 \cb3 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec2 function\cf0 \strokec4 \'a0processData(inputString)\'a0\{\cb1 \
+\pard\pardeftab720\partightenfactor0
+\cf0 \cb3 \'a0\'a0\'a0\'a0\cf5 \strokec5 //\'a0This\'a0line\'a0of\'a0code\'a0prints\'a0the\'a0first\'a0line\'a0of\'a0output\cf0 \cb1 \strokec4 \
+\cb3 \'a0\'a0\'a0\'a0console.log(\cf6 \strokec6 "Hello,\'a0World."\cf0 \strokec4 );\cb1 \
+\cb3 \'a0\'a0\'a0\'a0\cb1 \
+\cb3 \'a0\'a0\'a0\'a0\cf5 \strokec5 //\'a0Write\'a0the\'a0second\'a0line\'a0of\'a0output\'a0that\'a0prints\'a0the\'a0contents\'a0of\'a0'inputString'\'a0here.\cf0 \cb1 \strokec4 \
+\cb3 \'a0\'a0\'a0\'a0console.log(inputString)\cb1 \
+\cb3 \}\cb1 \
+\
+}
